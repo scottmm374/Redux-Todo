22c16bb0389fe56a1d5e65c86049f7224ea412ec
Michelle Scott <scottmm374@gmail.com>
2019-07-15 18:30:46 -0400

    Added TodoList to display tasks

--- todo/src/containers/ToDoList.js
+++ todo/src/containers/ToDoList.js
@@ -1,7 +1,34 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { Container } from 'reactstrap';
-import { addTodo } from '../Actions/index';
+import { Container, ListGroup, ListGroupItem } from 'reactstrap';
+
+function ToDoList(props) {
+    const {  task } = props
+    return (
+        <Container>
+            <ListGroup>
+                <ListGroupItem>
+
+                </ListGroupItem>
+                <ListGroupItem>
+                    { task.map (( task, i) => {
+                        return <ListGroupItem key={i}>{task}</ListGroupItem>
+                    })}
+                </ListGroupItem>
+            </ListGroup>
+
+        </Container>
+    )
+}
+
+const mapStateToProps = (state) => {
+    return{
+        task: state.task,
+        id: state.id
+    }
+}
+
+export default connect(mapStateToProps)(ToDoList)
 
 
 
